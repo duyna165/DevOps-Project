e50fa36dcc59168c261eb97467ca6560ac2b4eed
duyna165 <61420358+duyna165@users.noreply.github.com>
2023-03-24 10:04:38 +0700

    Update backend.tf

--- DevOps_Project_3/Terraform/backend.tf
+++ DevOps_Project_3/Terraform/backend.tf
@@ -2,8 +2,8 @@
 
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile"
-    key = "eks/ngg_cluster_name/statefile"
+    bucket = "duyna11-bucket"
+    key = "eks/duyna_cluster_name/statefile"
     region = "ap-south-1"
   }
 } 
